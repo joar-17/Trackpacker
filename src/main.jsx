35a1921f7b0e7efa5014f2c5d6533b9c1c072f9f
joar-17 <joararnstrom@gmail.com>
2026-06-27 17:16:05 +0200

    uppdate to display marker

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
